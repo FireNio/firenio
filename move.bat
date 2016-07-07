3ee5cb98061e10018259b38baaf643437778f4af
wangkai <8738115@qq.com>
2016-07-07 09:55:48 +0800

    * 准备尝试：ByteBuffer使用对象池

--- move.bat
+++ move.bat
@@ -1,6 +1,6 @@
-copy .\target\nimbleio-1.1-SNAPSHOT.jar .\release\NimbleIO\app
-copy .\target\nimbleio-1.1-SNAPSHOT.jar .\release\NimbleIO\lib
-copy .\target\nimbleio-1.1-SNAPSHOT-sources.jar .\release\NimbleIO\lib
+copy .\target\nimbleio-0.0.1-SNAPSHOT.jar .\release\NimbleIO\app
+copy .\target\nimbleio-0.0.1-SNAPSHOT.jar .\release\NimbleIO\lib
+copy .\target\nimbleio-0.0.1-SNAPSHOT-sources.jar .\release\NimbleIO\lib
 copy .\target\classes\*.properties .\release\NimbleIO\conf
 copy .\target\classes\*.cfg .\release\NimbleIO\conf
 copy .\target\classes\permission\*.cfg .\release\NimbleIO\conf\permission\
