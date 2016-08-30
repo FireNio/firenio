13388894264460f6e21cb9ca46e3bf6409ed7ccc
wangkai <8738115@qq.com>
2016-08-30 15:45:29 +0800

    * move 命令文件路径有误

--- nimbleio/move.bat
+++ nimbleio/move.bat
@@ -4,7 +4,7 @@ copy ..\nimbleio-core\target\*SNAPSHOT.jar ..\release\nimble-io.nio\lib
 copy ..\nimbleio-extend\target\*SNAPSHOT.jar ..\release\nimble-io.http\lib
 copy ..\nimbleio-extend\target\*SNAPSHOT.jar ..\release\nimble-io.nio\lib
 
-xcopy ..\nimbleio-core\src\main\resources\http\* ..\release\nimble-io.http\ /d/e
+xcopy ..\nimbleio-extend\src\main\resources\http\* ..\release\nimble-io.http\ /d/e
 xcopy ..\nimbleio-extend\src\main\resources\nio\* ..\release\nimble-io.nio\ /d/e
 
 mvn install:install-file -DgroupId=com.gifisan -DartifactId=nimbleio-core -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -Dfile=..\nimbleio-core\target\nimbleio-core-0.0.1-SNAPSHOT.jar
