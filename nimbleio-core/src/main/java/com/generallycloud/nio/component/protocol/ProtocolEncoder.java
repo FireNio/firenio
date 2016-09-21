679f4b3547558d5d52fde5df90beb5ebf34435a9
wangkai <8738115@qq.com>
2016-09-21 14:24:31 +0800

    * add images

--- nimbleio-core/src/main/java/com/generallycloud/nio/component/protocol/ProtocolEncoder.java
+++ nimbleio-core/src/main/java/com/generallycloud/nio/component/protocol/ProtocolEncoder.java
@@ -6,7 +6,13 @@ import com.generallycloud.nio.component.TCPEndPoint;
 
 public interface ProtocolEncoder {
 
-	//FIXME encode 失败要做release
+	/**
+	 * 注意：encode失败要release掉encode过程中申请的内存
+	 * @param endPoint
+	 * @param future
+	 * @return
+	 * @throws IOException
+	 */
 	public abstract IOWriteFuture encode(TCPEndPoint endPoint,IOReadFuture future) throws IOException;
 
 }
