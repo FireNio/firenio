ec1ba3d49bd5d69cde5013254c91e2501708ed07
wangkai <8738115@qq.com>
2016-09-27 14:53:43 +0800

    * 打包生成源码

--- nimbleio/install-jar-core.bat
+++
@@ -1,1 +0,0 @@
-mvn install:install-file -DgroupId=com.generallycloud -DartifactId=nimbleio-core -Dversion=1.0.8-SNAPSHOT -Dpackaging=jar -Dfile=..\nimbleio-core\target\nimbleio-core-1.0.8-SNAPSHOT.jar
--- nimbleio/move-resources.bat
+++ nimbleio/move-resources.bat
@@ -1,10 +1,11 @@
-copy ..\nimbleio-core\target\*SNAPSHOT.jar ..\nimbleio-release\nimble-io.http\lib
-copy ..\nimbleio-core\target\*SNAPSHOT.jar ..\nimbleio-release\nimble-io.nio\lib
+copy ..\nimbleio-all\target\nimbleio-all*SNAPSHOT.jar ..\nimbleio-release\nimble-io.http\lib
+copy ..\nimbleio-all\target\nimbleio-all*SNAPSHOT.jar ..\nimbleio-release\nimble-io.nio\lib
 
-copy ..\nimbleio-extend\target\*SNAPSHOT.jar ..\nimbleio-release\nimble-io.http\lib
-copy ..\nimbleio-extend\target\*SNAPSHOT.jar ..\nimbleio-release\nimble-io.nio\lib
-copy ..\nimbleio-all\target\*SNAPSHOT.jar .
-copy ..\nimbleio-core\target\*sources.jar .
+copy ..\nimbleio-all\target\*source.zip ..\nimbleio-release\nimble-io.http\lib
+copy ..\nimbleio-all\target\*source.zip ..\nimbleio-release\nimble-io.nio\lib
+
+copy ..\nimbleio-all\target\nimbleio-all*SNAPSHOT.jar .
+copy ..\nimbleio-all\target\*source.zip .
 
 xcopy ..\nimbleio-test\src\main\resources\http\* ..\nimbleio-release\nimble-io.http\ /d/e
 xcopy ..\nimbleio-test\src\main\resources\nio\* ..\nimbleio-release\nimble-io.nio\ /d/e
