a516d53cc98da25f536640a2bed175d2e919b447
wangkai <8738115@qq.com>
2016-10-20 09:34:44 +0800

    * rename

--- baseio/move-resources.bat
+++ baseio/move-resources.bat
@@ -1,11 +1,11 @@
-copy ..\baseio-all\target\baseio-all*SNAPSHOT.jar ..\baseio-release\base-io.http\lib
-copy ..\baseio-all\target\baseio-all*SNAPSHOT.jar ..\baseio-release\base-io.nio\lib
+copy ..\baseio-all\target\baseio-all*SNAPSHOT.jar ..\baseio-release\baseio.http\lib
+copy ..\baseio-all\target\baseio-all*SNAPSHOT.jar ..\baseio-release\baseio.nio\lib
 
-copy ..\baseio-all\target\*source.jar ..\baseio-release\base-io.http\lib
-copy ..\baseio-all\target\*source.jar ..\baseio-release\base-io.nio\lib
+copy ..\baseio-all\target\*source.jar ..\baseio-release\baseio.http\lib
+copy ..\baseio-all\target\*source.jar ..\baseio-release\baseio.nio\lib
 
 copy ..\baseio-all\target\baseio-all*SNAPSHOT.jar .
 copy ..\baseio-all\target\*source.jar .
 
-xcopy ..\baseio-test\src\main\resources\http\* ..\baseio-release\base-io.http\ /d/e
-xcopy ..\baseio-test\src\main\resources\nio\* ..\baseio-release\base-io.nio\ /d/e
+xcopy ..\baseio-test\src\main\resources\http\* ..\baseio-release\baseio.http\ /d/e
+xcopy ..\baseio-test\src\main\resources\nio\* ..\baseio-release\baseio.nio\ /d/e
