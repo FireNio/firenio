cddb905c681c5729b25b30ff04cd24d80259c44a
wangkai <8738115@qq.com>
2016-10-31 10:10:12 +0800

    * 清理不需要的jar包

--- baseio-release/baseio.nio/nio-startup.bat
+++ baseio-release/baseio.nio/nio-startup.bat
@@ -1,1 +1,1 @@
-java -Xms128m -Xmx512m -verbose:gc -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6667,suspend=n -cp .;.\lib\*; com.generallycloud.nio.extend.startup.NIOServerStartup
+java -Xms128m -Xmx512m -verbose:gc -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6667,suspend=n -cp .;.\lib\*; com.generallycloud.nio.extend.startup.BaseServerStartup
