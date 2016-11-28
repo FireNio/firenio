8ef2ed0626ee4f97055374231ec5add82241e7ee
wangkai <8738115@qq.com>
2016-11-28 16:06:52 +0800

    * 取消远程调试

--- baseio-test/http-startup.cmd
+++ baseio-test/http-startup.cmd
@@ -8,6 +8,6 @@ call mvn clean compile
 
 rem call mvn exec:java -Dexec.mainClass="com.generallycloud.test.nio.http11.TestHTTPServer"
 
-set MAVEN_OPTS=-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n
+rem set MAVEN_OPTS=-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n
 
 call mvn exec:java -Dexec.mainClass="com.generallycloud.test.nio.http11.TestHTTPServer"
