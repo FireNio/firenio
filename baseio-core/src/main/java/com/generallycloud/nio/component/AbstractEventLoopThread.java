2379febab3649b99f3f4fa5ddd8522781a82f950
wangkai <8738115@qq.com>
2016-11-30 11:07:23 +0800

    * wait for free 时检查working状态

--- baseio-core/src/main/java/com/generallycloud/nio/component/AbstractEventLoopThread.java
+++ baseio-core/src/main/java/com/generallycloud/nio/component/AbstractEventLoopThread.java
@@ -6,137 +6,142 @@ import com.generallycloud.nio.component.concurrent.EventLoopThread;
 
 public abstract class AbstractEventLoopThread implements EventLoopThread {
 
-	private static final Logger logger = LoggerFactory.getLogger(AbstractEventLoopThread.class);
-	
-	private volatile boolean running = false;
-	
-	private boolean working = false;
-	
-	private boolean stoping = false;
-	
-	private byte[] workingLock = new byte[]{};
-	
-	private Thread			monitor;
-	
+	private static final Logger	logger			= LoggerFactory.getLogger(AbstractEventLoopThread.class);
+
+	private volatile boolean		running		= false;
+
+	private boolean				working		= false;
+
+	private boolean				stoping		= false;
+
+	private byte[]				workingLock	= new byte[] {};
+
+	private Thread					monitor 		= null;
+
 	public void loop() {
-		
-		for(;;){
-			
+
+		for (;;) {
+
 			if (!running) {
 				notify4Free();
 				return;
 			}
-			
+
 			working = true;
-			
+
 			if (stoping) {
 				working = false;
 				notify4Free();
 				return;
 			}
-			
+
 			try {
-				
+
 				doLoop();
-				
+
 			} catch (Throwable e) {
-				logger.error(e.getMessage(),e);
+				logger.error(e.getMessage(), e);
 			}
-			
+
 			working = false;
 		}
 	}
-	
+
 	protected abstract void doLoop();
-	
-	protected void beforeStop(){}
+
+	protected void beforeStop() {
+	}
 
 	public void stop() {
-		
+
 		synchronized (this) {
-			
+
 			if (!running) {
 				return;
 			}
-			
+
 			try {
 				beforeStop();
 			} catch (Exception e) {
-				logger.error(e.getMessage(),e);
+				logger.error(e.getMessage(), e);
 			}
-			
+
 			running = false;
-			
+
 			stoping = true;
-			
+
 			if (working) {
-				
+
 				notify4Free();
-				
+
 				wakeupThread();
-				
+
 				wait4Free();
 			}
-			
+
 			stoping = false;
-			
+
 			doStop();
 		}
 	}
-	
-	protected void doStop(){}
-	
-	private void wait4Free(){
+
+	protected void doStop() {
+	}
+
+	private void wait4Free() {
 		synchronized (workingLock) {
-			try {
-				workingLock.wait();
-			} catch (InterruptedException e) {
-				logger.error(e.getMessage(),e);
+			if (working) {
+				try {
+					workingLock.wait();
+				} catch (InterruptedException e) {
+					logger.error(e.getMessage(), e);
+				}
 			}
 		}
 	}
-	
-	private void notify4Free(){
+
+	private void notify4Free() {
 		synchronized (workingLock) {
 			workingLock.notify();
 		}
 	}
 
-	protected void sleep(long time){
+	protected void sleep(long time) {
 		synchronized (workingLock) {
 			try {
 				workingLock.wait(time);
 			} catch (InterruptedException e) {
-				logger.error(e.getMessage(),e);
+				logger.error(e.getMessage(), e);
 			}
 		}
 	}
-	
-	protected void wakeupThread(){}
+
+	protected void wakeupThread() {
+	}
 
 	public void startup(String threadName) throws Exception {
-		
+
 		synchronized (this) {
-		
+
 			if (running) {
 				return;
 			}
-			
+
 			running = true;
-		
+
 			this.monitor = new Thread(new Runnable() {
-	
+
 				public void run() {
 					loop();
 				}
 			}, threadName);
-			
+
 			this.doStartup();
-			
+
 			this.monitor.start();
 		}
 	}
-	
+
 	public boolean isMonitor(Thread thread) {
 		return monitor == thread;
 	}
@@ -144,9 +149,9 @@ public abstract class AbstractEventLoopThread implements EventLoopThread {
 	public Thread getMonitor() {
 		return monitor;
 	}
-	
-	protected void doStartup() throws Exception{
-		
+
+	protected void doStartup() throws Exception {
+
 	}
 
 	public boolean isRunning() {
@@ -156,5 +161,5 @@ public abstract class AbstractEventLoopThread implements EventLoopThread {
 	public boolean isStopping() {
 		return stoping;
 	}
-	
+
 }
