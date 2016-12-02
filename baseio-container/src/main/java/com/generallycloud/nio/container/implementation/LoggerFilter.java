f793f2e46be8f09ef9e326767c141425da1cf3fc
wangkai <8738115@qq.com>
2016-12-02 15:12:09 +0800

    * 扩展日志记录 filter

--- baseio-container/src/main/java/com/generallycloud/nio/container/implementation/LoggerFilter.java
+++ baseio-container/src/main/java/com/generallycloud/nio/container/implementation/LoggerFilter.java
@@ -2,6 +2,7 @@ package com.generallycloud.nio.container.implementation;
 
 import com.generallycloud.nio.common.Logger;
 import com.generallycloud.nio.common.LoggerFactory;
+import com.generallycloud.nio.common.StringUtil;
 import com.generallycloud.nio.component.SocketSession;
 import com.generallycloud.nio.container.service.FutureAcceptorFilter;
 import com.generallycloud.nio.protocol.NamedReadFuture;
@@ -12,8 +13,23 @@ public class LoggerFilter extends FutureAcceptorFilter {
 
 	protected void accept(SocketSession session, NamedReadFuture future) throws Exception {
 
-		logger.info("请求IP：{}，服务名称：{}，请求内容：{}",
-				new String[] { session.getRemoteAddr(), future.getFutureName(), future.getReadText() });
+		String futureName = future.getFutureName();
+
+		if ("/favicon.ico".equals(futureName)) {
+			return;
+		}
+
+		String remoteAddr = session.getRemoteAddr();
+
+		String readText = future.getReadText();
+
+		if (!StringUtil.isNullOrBlank(readText)) {
+
+			logger.info("请求IP：{}，服务名称：{}，请求内容：{}", new String[] { remoteAddr, futureName, readText });
+			return;
+		}
+
+		logger.info("请求IP：{}，服务名称：{}",remoteAddr, futureName);
 	}
 
 }
