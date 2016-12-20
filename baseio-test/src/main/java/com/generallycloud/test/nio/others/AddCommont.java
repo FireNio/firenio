2fa3a0de26cd3c3b21eeeb9b86714c41c8c65784
wangkai <8738115@qq.com>
2016-12-20 10:12:37 +0800

    * add license header

---
+++ baseio-test/src/main/java/com/generallycloud/test/nio/others/AddCommont.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2015 GenerallyCloud.com
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package com.generallycloud.test.nio.others;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.generallycloud.nio.Encoding;
+import com.generallycloud.nio.common.FileUtil;
+
+/**
+ * @author wangkai
+ *
+ */
+public class AddCommont {
+
+	public static void main(String[] args) throws IOException {
+
+		String commont = "/*" + "\n * Copyright 2015 GenerallyCloud.com" + "\n *  "
+				+ "\n * Licensed under the Apache License, Version 2.0 (the \"License\");"
+				+ "\n * you may not use this file except in compliance with the License."
+				+ "\n * You may obtain a copy of the License at" + "\n *  "
+				+ "\n *      http://www.apache.org/licenses/LICENSE-2.0" + "\n *  "
+				+ "\n * Unless required by applicable law or agreed to in writing, software"
+				+ "\n * distributed under the License is distributed on an \"AS IS\" BASIS,"
+				+ "\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
+				+ "\n * See the License for the specific language governing permissions and"
+				+ "\n * limitations under the License." + "\n */ \n";
+
+		
+		System.out.println(commont);
+		
+		addCommont(new File("D:/GIT/baseio-master/baseio"), commont);
+		
+	}
+	
+	static void addCommont(File file,String c) throws IOException{
+		
+		if (!file.exists()) {
+			return;
+		}
+		
+		if (file.isDirectory()) {
+			
+			File [] fs = file.listFiles(); 
+			
+			for(File f:fs){
+				addCommont(f, c);
+			}
+			
+		}else{
+			if (file.getName().endsWith(".java")) {
+				
+				String content = FileUtil.readFileToString(file, Encoding.UTF8);
+				
+				content = c + content;
+				
+				FileUtil.write(file, content.getBytes(Encoding.UTF8),false);
+				
+				System.out.println("File:"+file.getAbsolutePath());
+			}
+		}
+	}
+
+}
--- baseio-test/src/main/java/com/generallycloud/test/nio/others/Test1.java
+++ baseio-test/src/main/java/com/generallycloud/test/nio/others/Test1.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 GenerallyCloud.com
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 package com.generallycloud.test.nio.others;
 
 //一共有7*3=21个格子，要随机选取其中16个格子，一共有多少种可能，用程序表示出来
