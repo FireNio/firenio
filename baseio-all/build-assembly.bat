8f599edb10c9d28b5da1804973ba38fe0c99d909
wangkai <8738115@qq.com>
2017-01-20 12:16:37 +0800

    * 貌似可以deploy了

--- baseio-all/build-assembly.bat
+++
@@ -1,1 +0,0 @@
-mvn clean assembly:assembly -DskipTests
--- baseio/project-build.bat
+++ baseio/project-build.bat
@@ -1,9 +1,6 @@
 
 call cmd /c build-package.bat
 
-rem cd ..\baseio-all
-rem call cmd /c build-assembly.bat
-
 cd ..\baseio
 call cmd /c .\move-resources.bat 
 
