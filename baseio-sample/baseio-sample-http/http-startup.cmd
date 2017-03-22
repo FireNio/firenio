360c829003c4eb5d725fbdde00e2ab6b911568fe
wangkai <8738115@qq.com>
2017-03-22 13:41:40 +0800

    * add intf.properties

---
+++ baseio-sample/baseio-sample-http/http-startup.cmd
@@ -0,0 +1,18 @@
+cd ../../baseio
+call build-package.bat
+
+cd ../baseio-sample
+call mvn clean install -DskipTests
+
+cd ../baseio-sample/baseio-sample-http
+call mvn clean package -DskipTests
+
+rem copy ..\baseio-sample\target\baseio-sample*.jar ..\baseio-test\target\classes\http\app\_java_lib\
+
+rem cd ../baseio-test
+
+rem set MAVEN_OPTS=-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n
+
+rem call mvn exec:java -Dexec.mainClass="com.generallycloud.baseio.sample.http11.startup.HttpBootstrap" -Dexec.args="http"  
+
+call mvn exec:java -Dexec.mainClass="com.generallycloud.baseio.sample.http11.startup.HttpBootstrap" 
