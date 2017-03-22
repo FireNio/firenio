f08688bd5cf422f06741eee9faf9c08178c44a10
wangkai <8738115@qq.com>
2017-03-22 11:13:32 +0800

    * 改进protobase容器启动

--- baseio-protobase/src/main/java/com/generallycloud/baseio/container/protobase/startup/ProtobaseServerStartup.java
+++ baseio-protobase/src/main/java/com/generallycloud/baseio/container/protobase/startup/ProtobaseServerStartup.java
@@ -19,6 +19,7 @@ import com.generallycloud.baseio.acceptor.SocketChannelAcceptor;
 import com.generallycloud.baseio.codec.protobase.ProtobaseProtocolFactory;
 import com.generallycloud.baseio.codec.protobase.future.ProtobaseBeatFutureFactory;
 import com.generallycloud.baseio.common.CloseUtil;
+import com.generallycloud.baseio.common.Encoding;
 import com.generallycloud.baseio.common.Logger;
 import com.generallycloud.baseio.common.LoggerFactory;
 import com.generallycloud.baseio.common.SharedBundle;
@@ -37,13 +38,13 @@ import com.generallycloud.baseio.container.service.FutureAcceptorServiceFilter;
 
 public class ProtobaseServerStartup {
 
-	private Logger		logger	= LoggerFactory.getLogger(ProtobaseServerStartup.class);
-
 	public void launch(String base) throws Exception {
+
+		SharedBundle bundle = SharedBundle.instance().loadAllProperties(base);
 		
-		SharedBundle.instance().loadAllProperties(base);
+		LoggerFactory.configure(bundle.loadProperties("conf/log4j.properties", Encoding.UTF8));
 		
-		ApplicationConfigurationLoader acLoader = new FileSystemACLoader();
+		ApplicationConfigurationLoader acLoader = new FileSystemACLoader(base);
 
 		ApplicationConfiguration ac = acLoader.loadConfiguration(SharedBundle.instance());
 		
@@ -74,11 +75,26 @@ public class ProtobaseServerStartup {
 			acceptor.bind();
 
 		} catch (Throwable e) {
+			
+			Logger logger = LoggerFactory.getLogger(getClass());
 
 			logger.error(e.getMessage(), e);
 
 			CloseUtil.unbind(acceptor);
 		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		ProtobaseServerStartup startup = new ProtobaseServerStartup();
+		
+		String base = null;
+		
+		if (args != null && args.length > 0) {
+			base = args[0];
+		}
+		
+		startup.launch(base);
 	}
 
 }
