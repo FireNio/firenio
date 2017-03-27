ef5c0f7b3d8dc6caf1f3e6523238e492ac037662
wangkai <8738115@qq.com>
2017-03-27 17:30:07 +0800

    * 分为打包resources和排除resources

--- baseio-sample/baseio-sample-http/http-startup.cmd
+++ baseio-sample/baseio-sample-http/http-startup.cmd
@@ -2,7 +2,7 @@ cd ../../baseio
 call build-package.bat
 
 cd ../baseio-sample/baseio-sample-http
-call mvn clean package -DskipTests
+call mvn clean package -P run -DskipTests
 
 rem copy ..\baseio-sample\target\baseio-sample*.jar ..\baseio-test\target\classes\http\app\_java_lib\
 
@@ -12,4 +12,4 @@ rem set MAVEN_OPTS=-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,s
 
 rem call mvn exec:java -Dexec.mainClass="com.generallycloud.baseio.sample.http11.startup.HttpBootstrap" -Dexec.args="http"  
 
-call mvn exec:java -Dexec.mainClass="com.generallycloud.baseio.container.startup.ApplicationBootstrap" 
+call mvn exec:java -Dexec.mainClass="com.generallycloud.baseio.container.startup.ApplicationBootstrap"
---
+++ baseio-sample/baseio-sample-http/test-release.bat
@@ -0,0 +1,4 @@
+call mvn clean package -P test-release -DskipTests
+
+cd ..
+pause
