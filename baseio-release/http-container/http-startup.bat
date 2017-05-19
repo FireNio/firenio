3bfc5dc8810577a3baae0ea4bcdfc505a6c7f552
wangkai <8738115@qq.com>
2017-05-19 15:07:27 +0800

    * update release

--- baseio-release/http-container/http-startup.bat
+++ baseio-release/http-container/http-startup.bat
@@ -1,4 +1,9 @@
 @rem >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 rem java -Xms128m -Xmx512m -verbose:gc -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -cp .;.\lib\*; com.generallycloud.nio.container.http11.startup.HttpServerStartup
+
+
+
+
 set "CURRENT_DIR=%cd%"
-java -cp ./lib/*; com.generallycloud.baseio.container.startup.ApplicationBootstrap %CURRENT_DIR%
+@rem java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %CURRENT_DIR% true
+java -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %CURRENT_DIR% true
