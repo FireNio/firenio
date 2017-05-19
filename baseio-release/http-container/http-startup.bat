164c59f52b76c69cf8935d771bf741a6a78a5d59
wangkai <8738115@qq.com>
2017-05-19 17:59:14 +0800

    * update startup command

--- baseio-release/http-container/http-startup.bat
+++ baseio-release/http-container/http-startup.bat
@@ -1,9 +1,15 @@
-@rem >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-rem java -Xms128m -Xmx512m -verbose:gc -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -cp .;.\lib\*; com.generallycloud.nio.container.http11.startup.HttpServerStartup
+cd ../../baseio
+call build-package.bat
 
+cd ../baseio-sample/baseio-sample-http
+call mvn clean package -P run -DskipTests
 
+xcopy target\classes\app ..\..\baseio-release\http-container\app\ /e /y
+xcopy target\baseio-sample-http-*-SNAPSHOT.jar ..\..\baseio-release\http-container\app\lib\ /y
+xcopy ..\..\baseio-all\target\baseio-all-*-SNAPSHOT.jar ..\..\baseio-release\http-container\lib\ /y
 
+cd ..\..\baseio-release\http-container
 
 set "CURRENT_DIR=%cd%"
-@rem java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %CURRENT_DIR% true
+rem java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %CURRENT_DIR% true
 java -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %CURRENT_DIR% true
