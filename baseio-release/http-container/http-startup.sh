64b2840439f88a771876d453646e2eab568d8a9d
wangkai <8738115@qq.com>
2017-05-23 13:30:50 +0800

    * update release sh

--- baseio-release/http-container/http-startup.sh
+++ baseio-release/http-container/http-startup.sh
@@ -1,3 +1,15 @@
+cd ../../baseio
+mvn clean install -DskipTests
+
+cd ../baseio-sample/baseio-sample-http
+mvn clean package -P run -DskipTests
+
+cp -r -u -v target/classes/app ../../baseio-release/http-container/app/ 
+cp -u -v target/baseio-sample-http-*-SNAPSHOT.jar ../../baseio-release/http-container/app/lib/ 
+cp -u -v ../../baseio-all/target/baseio-all-*-SNAPSHOT.jar ../../baseio-release/http-container/lib/ 
+
+cd ../../baseio-release/http-container
+
 PRG="$0"
 PRGDIR=`dirname "$PRG"`
-java -cp lib/baseio-all-3.1.8-SNAPSHOT.jar:lib/fastjson-1.1.41.jar:lib/log4j-1.2.14.jar:lib/slf4j-api-1.7.2.jar:lib/slf4j-log4j12-1.7.2.jar com.generallycloud.baseio.container.startup.ApplicationBootstrap $PRGDIR
+java -cp lib/baseio-all-3.1.8-SNAPSHOT.jar:lib/fastjson-1.1.41.jar:lib/log4j-1.2.14.jar:lib/slf4j-api-1.7.2.jar:lib/slf4j-log4j12-1.7.2.jar com.generallycloud.baseio.container.startup.ApplicationBootstrap $PRGDIR true
