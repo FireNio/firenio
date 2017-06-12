201ed71b0d315c6fa74bfbcbc9faaeed65a7e6c2
wangkai <8738115@qq.com>
2017-06-12 15:27:13 +0800

    * update to 3.1.11-SNAPSHOT

--- baseio-core/src/main/java/com/generallycloud/baseio/common/MD5Token.java
+++ baseio-core/src/main/java/com/generallycloud/baseio/common/MD5Token.java
@@ -12,17 +12,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 package com.generallycloud.baseio.common;
 
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 
 public class MD5Token {
-	
-	private static char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7',
-			'8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	private static MD5Token instance = new MD5Token();
+
+	private static char		hexDigits[]	= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'a', 'b', 'c', 'd', 'e', 'f' };
+	private static MD5Token	instance		= new MD5Token();
 
 	private MD5Token() {
 	}
@@ -31,35 +31,35 @@ public class MD5Token {
 		return instance;
 	}
 
-	public String getShortToken(String value,Charset encoding) {
-		return encoder(value,encoding).substring(8, 24);
+	public String getShortToken(String value, Charset encoding) {
+		return getLongToken(value.getBytes()).substring(8, 24);
 	}
 
-	public String getLongToken(String value,Charset encoding) {
-		return encoder(value,encoding).toString();
+	public String getLongToken(String value, Charset encoding) {
+		return getLongToken(value.getBytes()).toString();
 	}
 
-	private StringBuilder encoder(String value,Charset encoding) {
-		if (value == null) {
-			value = "";
-		}
-		MessageDigest md5 = null;
+	public String getLongToken(byte[] array) {
+		return getLongToken(array, 0, array.length);
+	}
+
+	public String getLongToken(byte[] array, int off, int len) {
 		try {
-			md5 = MessageDigest.getInstance("MD5");
-			md5.update(value.getBytes(encoding));
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			md5.update(array, off, len);
+			return toHex(md5.digest());
 		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage(),e);
+			throw new RuntimeException(e.getMessage(), e);
 		}
-		return toHex(md5.digest());
 	}
 
-	private StringBuilder toHex(byte[] bytes) {
-		StringBuilder str = new StringBuilder(32);
+	private String toHex(byte[] bytes) {
+		StringBuilder s = new StringBuilder(32);
 		int length = bytes.length;
 		for (int i = 0; i < length; i++) {
-			str.append(hexDigits[(bytes[i] & 0xf0) >> 4]);
-			str.append(hexDigits[bytes[i] & 0x0f]);
+			s.append(hexDigits[(bytes[i] & 0xf0) >> 4]);
+			s.append(hexDigits[bytes[i] & 0x0f]);
 		}
-		return str;
+		return s.toString();
 	}
 }
