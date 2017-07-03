0181470ef6706d63688bc0148b44b932279a1c1c
wangkai <8738115@qq.com>
2017-07-03 17:37:09 +0800

    * update startup.sh

--- baseio-release/http-container/http-startup.sh
+++ baseio-release/http-container/http-startup.sh
@@ -12,4 +12,4 @@ cd ../../baseio-release/http-container
 
 PRG="$0"
 PRGDIR=`dirname "$PRG"`
-java -cp lib/baseio-all-3.1.9-SNAPSHOT.jar:lib/fastjson-1.1.41.jar:lib/log4j-1.2.14.jar:lib/slf4j-api-1.7.2.jar:lib/slf4j-log4j12-1.7.2.jar com.generallycloud.baseio.container.startup.ApplicationBootstrap $PRGDIR true
+java -cp lib/baseio-all-3.1.11-SNAPSHOT.jar:lib/fastjson-1.1.41.jar:lib/log4j-1.2.14.jar:lib/slf4j-api-1.7.2.jar:lib/slf4j-log4j12-1.7.2.jar com.generallycloud.baseio.container.startup.ApplicationBootstrap $PRGDIR true
