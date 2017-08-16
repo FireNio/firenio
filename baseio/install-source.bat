cfc946e7dec1019d984ab867f3e08445ee05d6ce
wangkai <8738115@qq.com>
2017-08-16 17:00:48 +0800

    * update version to 3.2.2-SNAPSHOT

--- baseio/install-source.bat
+++ baseio/install-source.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=baseio-all-3.2.1-SNAPSHOT-sources.jar -DgroupId=com.generallycloud -DartifactId=baseio-all -Dversion=3.2.1-SNAPSHOT -Dpackaging=jar -Dclassifier=sources
+mvn install:install-file -Dfile=baseio-all-3.2.2-SNAPSHOT-sources.jar -DgroupId=com.generallycloud -DartifactId=baseio-all -Dversion=3.2.2-SNAPSHOT -Dpackaging=jar -Dclassifier=sources
--- baseio/project-v2snapshot.bat
+++ baseio/project-v2snapshot.bat
@@ -1,2 +1,2 @@
-rem mvn versions:set -DnewVersion=3.2.1-SNAPSHOT
-mvn versions:set -DnewVersion=3.2.1.RELEASE
+mvn versions:set -DnewVersion=3.2.2-SNAPSHOT
+rem mvn versions:set -DnewVersion=3.2.1.RELEASE
