83826b07aeee5003bf3bf25d5325bd0b4dfa7a97
wangkai <8738115@qq.com>
2017-09-04 10:24:37 +0800

    * add no build startup command

---
+++ baseio-release/http-container/http-startup-nobuild.bat
@@ -0,0 +1,1 @@
+java -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %cd% true
---
+++ baseio-release/http-container/http-startup-nobuild.sh
@@ -0,0 +1,10 @@
+lsof -ntP -i:443|xargs kill -9
+
+CLASSPATH=""
+for i in lib/*.jar; do
+   CLASSPATH="$CLASSPATH":"$i"
+done
+
+PRG="$0"
+PRGDIR=`dirname "$PRG"`
+java -cp $CLASSPATH com.generallycloud.baseio.container.startup.ApplicationBootstrap $PRGDIR true
