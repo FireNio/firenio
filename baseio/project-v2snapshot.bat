eb1667803229e16b6f3d6dbac152c892240b7695
wangkai <8738115@qq.com>
2017-10-27 10:29:20 +0800

    * update version to 3.2.3.Release

--- baseio/project-v2snapshot.bat
+++ baseio/project-v2snapshot.bat
@@ -1,2 +1,2 @@
-mvn versions:set -DnewVersion=3.2.3-SNAPSHOT
-rem mvn versions:set -DnewVersion=3.2.2.RELEASE
+rem mvn versions:set -DnewVersion=3.2.3-SNAPSHOT
+mvn versions:set -DnewVersion=3.2.3.RELEASE
