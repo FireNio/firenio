a79f1cbba59a8d09b13974987c877d42c0e83b94
wangkai <8738115@qq.com>
2018-01-15 14:11:59 +0800

    * add aes encrypt

---
+++ baseio-core/src/main/java/com/generallycloud/baseio/common/AESUtil.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2015-2017 GenerallyCloud.com
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.generallycloud.baseio.common;
+
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author wangkai
+ *
+ */
+public class AESUtil {
+
+    public static String encrypt(String content, String password) throws Exception {
+        return encrypt(content, password, Encoding.UTF8);
+    }
+
+    public static String encrypt(String content, String password, Charset charset)
+            throws Exception {
+        byte[] contentBytes = content.getBytes(charset);
+        byte[] passwordBytes = password.getBytes(charset);
+        byte[] res = encrypt(contentBytes, passwordBytes);
+        return MathUtil.bytes2HexString(res);
+    }
+
+    public static byte[] encrypt(byte[] content, byte[] password) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");// 创建AES的Key生产者
+        // 利用用户密码作为随机数初始化出,128位的key生产者
+        //加密没关系，SecureRandom是生成安全随机数序列，password.getBytes()是种子，只要种子相同，序列就一样，所以解密只要有password就行
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        random.setSeed(password);
+        kgen.init(128, random);
+        SecretKey secretKey = kgen.generateKey();// 根据用户密码，生成一个密钥
+        // 返回基本编码格式的密钥，如果此密钥不支持编码，则返回null
+        byte[] enCodeFormat = secretKey.getEncoded();
+        // 转换为AES专用密钥
+        SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+        // 创建密码器
+        Cipher cipher = Cipher.getInstance("AES");
+        // 初始化为加密模式的密码器
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        // 加密
+        return cipher.doFinal(content);
+    }
+
+    public static byte[] decrypt(byte[] content, byte[] password) throws Exception {
+        // 创建AES的Key生产者
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        random.setSeed(password);
+        kgen.init(128, random);
+        // 根据用户密码，生成一个密钥
+        SecretKey secretKey = kgen.generateKey();
+        // 返回基本编码格式的密钥
+        byte[] enCodeFormat = secretKey.getEncoded();
+        // 转换为AES专用密钥
+        SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+        // 创建密码器
+        Cipher cipher = Cipher.getInstance("AES");
+        // 初始化为解密模式的密码器
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(content);
+    }
+
+    public static String decrypt(String content, String password) throws Exception {
+        return decrypt(content, password, Encoding.UTF8);
+    }
+
+    public static String decrypt(String content, String password, Charset charset)
+            throws Exception {
+        byte[] contentBytes = MathUtil.hexString2bytes(content.toCharArray());
+        byte[] passowrdBytes = password.getBytes(charset);
+        byte[] res = decrypt(contentBytes, passowrdBytes);
+        return new String(res, charset);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+        String text = "test";
+        String key = "key";
+        System.out.println("TEXT:"+text);
+        System.out.println("KEY:"+key);
+        String mi = encrypt(text, key);
+        System.out.println("MI:"+mi);
+        text = decrypt(mi, key);
+        System.out.println("TEXT:"+text);
+        
+    }
+    
+
+}
--- baseio-core/src/main/java/com/generallycloud/baseio/common/MathUtil.java
+++ baseio-core/src/main/java/com/generallycloud/baseio/common/MathUtil.java
@@ -15,10 +15,11 @@
  */
 package com.generallycloud.baseio.common;
 
+import java.util.Random;
+
 public class MathUtil {
 
     public static byte binaryString2byte(String string) {
-
         Assert.notNull(string, "null binary string");
         char c0 = '0';
         char c1 = '1';
@@ -218,6 +219,44 @@ public class MathUtil {
         }
         return builder.toString();
     }
+    
+    public static byte[] hexString2bytes(char[] data) {
+        int len = data.length;
+        if ((len & 0x01) != 0) {
+            throw new RuntimeException("Odd number of characters.");
+        }
+        byte[] out = new byte[len >> 1];
+        // two characters form the hex value.
+        //49
+        //58
+        //98
+        //103
+        for (int i = 0; i < data.length;) {
+            int t = i >> 1;
+            int ch0 = hex2Digit(data[i], i++);
+            int ch1 = hex2Digit(data[i], i++);
+            out[t] = (byte)(ch0 << 4 | ch1);
+        }
+        return out;
+    }
+    
+    private static int hex2Digit(char ch,int index){
+        if (ch < 48) {
+            throw new RuntimeException("Illegal hexadecimal charcter " + ch + " at index " + index);
+        }else if(ch < 58){
+            return ch - 48;
+        }else if(ch < 65){
+            throw new RuntimeException("Illegal hexadecimal charcter " + ch + " at index " + index);
+        }else if(ch < 71){
+            return ch - 55;
+        }else if(ch < 97){
+            throw new RuntimeException("Illegal hexadecimal charcter " + ch + " at index " + index);
+        }else if(ch < 103){
+            return ch - 87;
+        }else{
+            throw new RuntimeException("Illegal hexadecimal charcter " + ch + " at index " + index);
+        }
+    }
 
     public static String bytes2HexString0X(byte[] array) {
         if (array == null || array.length == 0) {
@@ -400,36 +439,48 @@ public class MathUtil {
 
     public static void main(String[] args) {
 
-        int v = Integer.MIN_VALUE + 2147299999;
+        int v = new Random().nextInt(Integer.MAX_VALUE);
         byte[] bytes = new byte[4];
         int2Byte(bytes, v, 0);
 
-        System.out.println(bytes2HexString0X(bytes));
-        System.out.println(bytes2HexString(bytes));
+        String hex = bytes2HexString(bytes);
+        bytes = hexString2bytes(hex.toCharArray());
+        System.out.println(v);
+        System.out.println(hex);
         int v1 = byte2Int(bytes, 0);
 
         System.out.println(v1);
         System.out.println(v1 == v);
+        
+        System.out.println('0'+1);
+        System.out.println('9'+1);
+        System.out.println('a'+1);
+        System.out.println('f'+1);
+        System.out.println('A'+1);
+        System.out.println('F'+1);
+        
+        
+        
     }
 
     private static String[] HEXS = new String[] { "00", "01", "02", "03", "04", "05", "06", "07",
-            "08", "09", "0a", "0b", "0c", "0d", "0e", "0f", "10", "11", "12", "13", "14", "15",
-            "16", "17", "18", "19", "1a", "1b", "1c", "1d", "1e", "1f", "20", "21", "22", "23",
-            "24", "25", "26", "27", "28", "29", "2a", "2b", "2c", "2d", "2e", "2f", "30", "31",
-            "32", "33", "34", "35", "36", "37", "38", "39", "3a", "3b", "3c", "3d", "3e", "3f",
-            "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "4a", "4b", "4c", "4d",
-            "4e", "4f", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5a", "5b",
-            "5c", "5d", "5e", "5f", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
-            "6a", "6b", "6c", "6d", "6e", "6f", "70", "71", "72", "73", "74", "75", "76", "77",
-            "78", "79", "7a", "7b", "7c", "7d", "7e", "7f", "80", "81", "82", "83", "84", "85",
-            "86", "87", "88", "89", "8a", "8b", "8c", "8d", "8e", "8f", "90", "91", "92", "93",
-            "94", "95", "96", "97", "98", "99", "9a", "9b", "9c", "9d", "9e", "9f", "a0", "a1",
-            "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "aa", "ab", "ac", "ad", "ae", "af",
-            "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "ba", "bb", "bc", "bd",
-            "be", "bf", "c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "ca", "cb",
-            "cc", "cd", "ce", "cf", "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9",
-            "da", "db", "dc", "dd", "de", "df", "e0", "e1", "e2", "e3", "e4", "e5", "e6", "e7",
-            "e8", "e9", "ea", "eb", "ec", "ed", "ee", "ef", "f0", "f1", "f2", "f3", "f4", "f5",
-            "f6", "f7", "f8", "f9", "fa", "fb", "fc", "fd", "fe", "ff" };
+            "08", "09", "0A", "0B", "0C", "0D", "0E", "0F", "10", "11", "12", "13", "14", "15",
+            "16", "17", "18", "19", "1A", "1B", "1C", "1D", "1E", "1F", "20", "21", "22", "23",
+            "24", "25", "26", "27", "28", "29", "2A", "2B", "2C", "2D", "2E", "2F", "30", "31",
+            "32", "33", "34", "35", "36", "37", "38", "39", "3A", "3B", "3C", "3D", "3E", "3F",
+            "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "4A", "4B", "4C", "4D",
+            "4E", "4F", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5A", "5B",
+            "5C", "5D", "5E", "5F", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+            "6A", "6B", "6C", "6D", "6E", "6F", "70", "71", "72", "73", "74", "75", "76", "77",
+            "78", "79", "7A", "7B", "7C", "7D", "7E", "7F", "80", "81", "82", "83", "84", "85",
+            "86", "87", "88", "89", "8A", "8B", "8C", "8D", "8E", "8F", "90", "91", "92", "93",
+            "94", "95", "96", "97", "98", "99", "9A", "9B", "9C", "9D", "9E", "9F", "A0", "A1",
+            "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "AA", "AB", "AC", "AD", "AE", "AF",
+            "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "BA", "BB", "BC", "BD",
+            "BE", "BF", "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CA", "CB",
+            "CC", "CD", "CE", "CF", "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9",
+            "DA", "DB", "DC", "DD", "DE", "DF", "E0", "E1", "E2", "E3", "E4", "E5", "E6", "E7",
+            "E8", "E9", "EA", "EB", "EC", "ED", "EE", "EF", "F0", "F1", "F2", "F3", "F4", "F5",
+            "F6", "F7", "F8", "F9", "FA", "FB", "FC", "FD", "FE", "FF" };
 
 }
