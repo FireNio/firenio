4fa1219c7b999eb3e745e69d1e43ae1f01b697a2
wangkai <8738115@qq.com>
2018-05-03 09:23:06 +0800

    * undertow get form data sample

--- baseio-test/src/main/java/com/generallycloud/test/io/undertow/TestUndertow.java
+++ baseio-test/src/main/java/com/generallycloud/test/io/undertow/TestUndertow.java
@@ -15,10 +15,17 @@
  */
 package com.generallycloud.test.io.undertow;
 
+import com.generallycloud.baseio.common.Encoding;
+
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.form.FormData;
+import io.undertow.server.handlers.form.FormDataParser;
+import io.undertow.server.handlers.form.FormParserFactory;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
+import io.undertow.util.Methods;
 
 /**
  * @author wangkai
@@ -27,10 +34,30 @@ import io.undertow.util.Headers;
 public class TestUndertow {
 
     public static void main(final String[] args) {
+        FormParserFactory.Builder builder = FormParserFactory.builder();
+        builder.setDefaultCharset(Encoding.UTF8.name());
+        FormParserFactory formParserFactory = builder.build();
         Undertow server = Undertow.builder().addHttpListener(8080, "localhost")
                 .setHandler(new HttpHandler() { //设置HttpHandler回调方法  
+                    
+                    private boolean hasBody(HttpServerExchange exchange) {
+                        int length = (int) exchange.getRequestContentLength();
+                        if (length == 0) return false;  // if body is empty, skip reading
+
+                        HttpString method = exchange.getRequestMethod();
+                        return Methods.POST.equals(method) || Methods.PUT.equals(method) || Methods.PATCH.equals(method);
+                    }
+                    
                     @Override
                     public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        if (hasBody(exchange)) {    // parse body early, not process until body is read (e.g. for chunked), to save one blocking thread during read
+                            FormDataParser parser = formParserFactory.createParser(exchange);
+                            if (parser == null) {
+                                return;
+                            }
+                            FormData data = parser.parseBlocking();
+                            System.out.println(data);
+                        }
                         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
                         exchange.getResponseSender().send("Hello World");
                     }
