e5bbd152cf0f5b15061459f59b5caa340aeeb0cf
wangkai <8738115@qq.com>
2018-09-11 15:21:45 +0800

    * update http sample

--- baseio-release/http-container/http-startup.bat
+++ baseio-release/http-container/http-startup.bat
@@ -7,11 +7,11 @@ call mvn clean package -P run -DskipTests
 xcopy target\classes\app ..\..\baseio-release\http-container\app\ /e /y
 xcopy target\baseio-sample-http-*-SNAPSHOT.jar ..\..\baseio-release\http-container\app\lib\ /y
 xcopy ..\..\baseio-all\target\baseio-all-*-SNAPSHOT.jar ..\..\baseio-release\http-container\lib\ /y
-rem xcopy ..\..\baseio-all\target\baseio-all-*-SNAPSHOT.jar ..\..\baseio-release\http-container\app\lib\ /y
+xcopy ..\..\baseio-all\target\baseio-all-*-SNAPSHOT.jar ..\..\baseio-release\http-container\app\lib\ /y
 
 cd ..\..\baseio-release\http-container
 
 rem java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=6666,suspend=n -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %cd% true
 rem java -cp ./lib/*;  com.generallycloud.baseio.container.startup.ApplicationBootstrap %cd% true
 
-java -XX:+PrintGCDetails -Xloggc:gc.log -cp ./lib/*; -Dcontainer.runtime=prod com.generallycloud.baseio.container.http11.HttpStartup
+java -XX:+PrintGCDetails -Xloggc:gc.log -cp ./lib/*; -Dcontainer.runtime=prod -Dcom.generallycloud.baseio.develop.debug=true com.generallycloud.sample.baseio.http11.startup.TestHttpStartup
