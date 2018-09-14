37d4f84be88a4c6f34e3921ce23bafd8c723b582
wangkai <8738115@qq.com>
2018-09-14 14:04:13 +0800

    * update version to 3.2.8-SNAPSHOT

--- baseio-sample/baseio-sample-http/http-startup.bat
+++
@@ -1,2 +0,0 @@
-call mvn clean compile -DskipTests
-call mvn exec:java -Dexec.mainClass="com.generallycloud.sample.baseio.http11.startup.HttpStartup" -Dexec.args="false"
--- baseio-sample/baseio-sample-http/http-startup.sh
+++
@@ -1,2 +0,0 @@
-mvn clean compile -DskipTests
-mvn exec:java -Dexec.mainClass="com.generallycloud.sample.baseio.http11.startup.HttpStartup" -Dexec.args="false"
